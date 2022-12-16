c637d119c1c1678b3806077121b91c7898b26787
viniciusvilasbs <vinyvilas@gmail.com>
2022-12-16 10:12:07 -0300

    Docker containers deploy at Amazon

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/FakeEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/FakeEnvioEmailService.java
@@ -1,14 +1,21 @@
 package com.algaworks.algafood.infrastructure.service.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.algaworks.algafood.domain.service.EnvioEmailService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService{
+public class FakeEnvioEmailService implements EnvioEmailService {
 
+	@Autowired
+	private ProcessadorEmailTemplate processadorEmailTemplate;
+	
 	@Override
     public void enviar(Mensagem mensagem) {
         
-        String corpo = processarTemplate(mensagem);
+        String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 
         log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
     }
--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
@@ -23,7 +19,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	private EmailProperties emailProperties;
 	
 	@Autowired
-	private Configuration freemarkerConfig;
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -37,7 +33,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	}
 	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException{
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 		
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		
@@ -48,16 +44,5 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 		helper.setText(corpo, true);
 		
 		return mimeMessage;
-	}
-	
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-			Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
-			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(
-					template, mensagem.getVariaveis());
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		}
 	}
 }
